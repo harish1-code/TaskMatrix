52d2b8a19684b315dc42a0e790aa1255ba0b34aa
Harish kumbar <harishkumbar7799@gmail.com>
2025-11-06 09:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ model = genai.GenerativeModel("gemini-2.0-flash")
 DB_HOST = os.environ.get('DB_HOST', 'localhost')
 DB_NAME = os.environ.get('DB_NAME', 'nexusboard')
 DB_USER = os.environ.get('DB_USER', 'postgres')
-DB_PASS = os.environ.get('DB_PASS', 'Harish@123')
+DB_PASS = os.environ.get('DB_PASS', '')
 DB_PORT = os.environ.get('DB_PORT', '5432')
 
 def get_db_conn():
